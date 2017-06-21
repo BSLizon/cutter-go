21982e94513eb433c1c2c37ec359de4e1fa02081
bslizon <bslizon@gmail.com>
2017-06-22 05:09:39 +0800

    add func LengthBasedCutter

---
+++ cutter/length_based_cutter.go
@@ -0,0 +1,64 @@
+package cutter
+
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+)
+
+const (
+	PAYLOAD_LENGTH_SIZE = 4       /*len(uint32)*/
+	MAX_PAYLOAD_SIZE    = 1 << 20 /*1MB*/
+)
+
+func LengthBasedCutter(r io.Reader, payloadMaxLength uint32, payload []byte) error {
+	if payloadMaxLength > MAX_PAYLOAD_SIZE {
+		return errors.New("out of payloadMaxLength limit")
+	}
+
+	//read length
+	lenBuf := make([]byte, PAYLOAD_LENGTH_SIZE)
+	var lengthBufIdx int
+	for {
+		n, err := r.Read(lenBuf[lengthBufIdx:])
+		if err != nil {
+			if err == io.EOF {
+				return io.EOF
+			}
+			return err
+		}
+		lengthBufIdx += n
+		if lengthBufIdx == PAYLOAD_LENGTH_SIZE {
+			break
+		} else if lengthBufIdx > PAYLOAD_LENGTH_SIZE || lengthBufIdx < 0 {
+			return errors.New("read payload length error")
+		}
+	}
+
+	length := binary.BigEndian.Uint32(lenBuf)
+	if length > payloadMaxLength {
+		return errors.New("payload length out of limit")
+	} else if length == 0 {
+		return errors.New("payload length equals 0")
+	}
+
+	//read payload
+	var payloadBufIdx uint32
+	for {
+		n, err := r.Read(payload[payloadBufIdx:])
+		if err != nil {
+			if err == io.EOF {
+				return io.EOF
+			}
+			return err
+		}
+
+		payloadBufIdx += uint32(n)
+
+		if payloadBufIdx == length {
+			return nil
+		} else if payloadBufIdx > length || payloadBufIdx < 0 {
+			return errors.New("read payload error")
+		}
+	}
+}
---
+++ cutter/length_based_cutter_test.go
@@ -0,0 +1,64 @@
+package cutter
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+	"testing"
+)
+
+func Test_LengthBasedCutter(t *testing.T) {
+	in := bytes.NewBuffer([]byte{})
+	lengthBuf := []byte{0x00, 0x00, 0x00, 0x00}
+	binary.BigEndian.PutUint32(lengthBuf, uint32(5))
+	in.Write(lengthBuf)
+	payload := []byte{0x00, 0x01, 0x10, 0x11, 0x19}
+	in.Write(payload)
+
+	out := []byte{0x00, 0x00, 0x00, 0x00, 0x00}
+	err := LengthBasedCutter(in, uint32(1<<10), out)
+	if err != nil {
+		if err == io.EOF {
+			goto Here1
+		}
+		t.Error("test failed.", err)
+	}
+
+Here1:
+	if payload == nil || out == nil {
+		t.Error("test failed.")
+	}
+
+	if len(payload) != len(out) {
+		t.Error("test failed.")
+	}
+
+	for i := range payload {
+		if payload[i] != out[i] {
+			t.Error("test failed.")
+		}
+	}
+
+	err = LengthBasedCutter(in, uint32(1<<10), out)
+	if err != nil {
+		if err == io.EOF {
+			goto Here2
+		}
+		t.Error("test failed.", err)
+	}
+
+Here2:
+	if payload == nil || out == nil {
+		t.Error("test failed.")
+	}
+
+	if len(payload) != len(out) {
+		t.Error("test failed.")
+	}
+
+	for i := range payload {
+		if payload[i] != out[i] {
+			t.Error("test failed.")
+		}
+	}
+}
